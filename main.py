346b1ce49f7ec0f94a579680c1f360c1f9f57026
Kevin Childs <kevinicus37@gmail.com>
2017-01-24 15:07:37 -0600

    Add Input Validation

--- main.py
+++ main.py
@@ -17,12 +17,12 @@
 import webapp2
 import helpers
 
-def page(name="", password="", verify="", email=""):
+def page(username ="", mail="", name="", password="", verify="", email=""):
     head="<head><style>.error { color: red;}</style></head>"
     header = "<h1>Signup</h1>"
     username_row = """<tr>
                       <td><label for='username'>Username</label></td>
-                      <td><input type='text' name='username' required>
+                      <td><input type='text' name='username' value='%(username)s' required>
                       <span class='error'>%(name)s</span>
                       </td>
                       </tr>"""
@@ -37,19 +37,56 @@ def page(name="", password="", verify="", email=""):
                     </td>
                     </tr>"""
     email_row = """<tr><td><label for='email'>Email (optional)</label></td>
-                   <td><input type='email' name='email' optional>
+                   <td><input type='email' name='email' value ='%(mail)s' optional>
                    <span class='error'>%(email)s</span>
                    </td>
                    </tr>"""
 
     form = "<body>" + head + header + "<form method='post'><table>" + username_row + password_row + verify_row + email_row + "</table><input type='submit'></form>"+ "</body>"
 
-    return form % {'name': name, 'password': password, 'verify': verify, 'email': email}
+    return form % {'username': username, 'mail':mail, 'name': name, 'password': password, 'verify': verify, 'email': email}
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write(page())
 
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password= self.request.get('password')
+        verify = self.request.get('verify_password')
+        email = self.request.get('email')
+
+        params = dict(username = username, mail = email)
+
+        if not helpers.valid_username(username):
+            params['name']="That's not a valid username."
+            have_error = True
+
+        if not helpers.valid_password(password):
+            params['password']="That's wasn't a valid password."
+            have_error = True
+        elif password != verify:
+            params['verify'] = "Your passwords did not match."
+            have_error = True
+
+        if not helpers.valid_email(email):
+            params['email'] = "That is not a valid email, stupid."
+            have_error = True
+
+        if have_error:
+            self.response.write(page(**params))
+        else:
+            self.redirect('/welcome?username=' + username)
+
+class Welcome(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get('username')
+        if helpers.valid_username(username):
+            self.response.write('Welcome, ' + username + '!')
+        else:
+            self.redirect('/')
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler), ('/welcome', Welcome)
 ], debug=True)
